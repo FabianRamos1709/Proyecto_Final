0b31bcc072438c383082ed632ffd773f0bad431c
unknown <fabianchors@gmail.com>
2024-05-14 19:37:08 -0500

    modificando url

--- src/components/FormaBuscador.jsx
+++ src/components/FormaBuscador.jsx
@@ -37,8 +37,7 @@ const FormaBuscador = ({setCharacters}) => {
     
   useEffect( () =>{
     const consultarApi = async() => {
-        const numbers = Array.from({ length: 100 }, (_, index) => index + 1);
-        const url = `https://rickandmortyapi.com/api/character/${numbers}`
+        const url = `https://rickandmortyapi.com/api/character/1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,40,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70`
         const respuesta = await fetch(url)
         const resultado = await respuesta.json()
     
